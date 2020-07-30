7ae1b76f932162fc42e6a60fb79fee324c493ab2
Cleiton Nunes <cleiton.rodrigues6102@gmail.com>
2020-07-30 12:29:22 -0300

    fix: LengthValidator

--- src/validators/helpers/length.validator.ts
+++ src/validators/helpers/length.validator.ts
@@ -1,8 +1,13 @@
 export class LengthValidator {
-	public static validate(content: string, length: number): boolean {
-		if (content.length !== length) {
-			return false;
+	public static validate(content: string, length: number | number[]): void {
+		if (Array.isArray(length) && !length.includes(content.length)) {
+			throw new Error(
+				`A linha digitável deve ter ${length.map((item, index) =>
+					index === length.length - 1 ? ` ou ${item}` : `${item}`,
+				)} caracteres`,
+			);
+		} else if (Number(length) && content.length !== Number(length)) {
+			throw new Error(`A linha digitável deve ter ${length} caracteres`);
 		}
-		return true;
 	}
 }
